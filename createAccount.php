ed3361b162347d2c180e12102509762c4d5fb82e
dolwijit13 <dol51890@gmail.com>
2021-03-03 15:38:35 +0700

    Seperate each method

---
+++ createAccount.php
@@ -0,0 +1,15 @@
+<?php
+    function getSafe($key){
+        if(!array_key_exists($key, $_POST))
+            return false;
+        return trim($_POST[$key]);
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] === 'POST')
+    {
+        $username = getSafe('username');
+        $password = getSafe('password');
+        
+        echo 'createAccount';
+    }
+?>
